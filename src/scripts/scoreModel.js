87e163d91dd02a4b4577cbda6cefa04c8b62c17a
Valentin Rangelov <eneragee@gmail.com>
2015-07-26 16:52:13 +0300

    Updating scoreModel.js

--- src/scripts/scoreModel.js
+++ src/scripts/scoreModel.js
@@ -20,9 +20,9 @@ function scoreModel() {
 
         Object.defineProperties(playerInternal, {
             init: {
-                value: function (name, score) {
+                value: function (name) {
                     this.name = name;
-                    this.score = score;
+                    this.score = 0;
                     return this;
                 }
             },
@@ -31,7 +31,12 @@ function scoreModel() {
                     return this._name;
                 },
                 set: function (value) {
-                    // TODO: Add validators.............
+                   if(value.length < 3){
+                       throw new Error('The length of the name must be between 3-10 letters.');
+                   }
+                   if(value.length > 10){
+                       throw new Error('The length of the name must be between 3-10 letters.');
+                   }
                     this._name = value;
                 }
             },
@@ -40,7 +45,14 @@ function scoreModel() {
                     return this._score;
                 },
                 set: function (value) {
-                    // TODO: Add validators.............
+                    //check if it is type of number
+                    if(typeof value != "number"){
+                        throw new Error("Score must be a number.");
+                    }
+                    
+                    if(value < 0){
+                        throw new Error('Score cannot be negative.');
+                    }
                     this._score = value;
                 }
             }
@@ -50,32 +62,74 @@ function scoreModel() {
 
     // Scoreboard class, keeps information about all players, data can be saved in file
     scoreBoard = (function () {
-        var scoreBoardInternal = Object.create({});
+        var scoreBoardInternal = Object.create({}),
+            playersAndScores = [] ;
 
         Object.defineProperties(scoreBoardInternal, {
             init: {
                 value: function () {
                     // TODO: initialize players array (maybe get data from a file if available)
+                    playersAndScores = [];
+                    playersAndScores.push(JSON.parse(localStorage.getItem("scoreboard")));
+                    return playersAndScores;
                 }
             },
             load: {
                 value: function () {
                     // TODO: load players JSON object from a file
+                    playersAndScores = [];
+                    playersAndScores.push(JSON.parse(localStorage.getItem("scoreboard")));
+                    return playersAndScores;
+                    
                 }
             },
             save: {
-                value: function () {
+                value: function (player) {
+                    var newPlayer,
+                        playerExists = false,
+                        everyPlayer;
                     // TODO: Save players JSON object to a file
+                    
+                    // Checks if such a player exists, and if so checks if current score is more than last saved to update it.
+                    playersAndScores = [];
+                    playersAndScores.push(JSON.parse(localStorage.getItem("scoreboard")));
+                    for (everyPlayer in playersAndScores){
+                        if(player.name == playersAndScores[everyPlayer].name){
+                            playerExists = true;
+                            if(player.score > playersAndScores[everyPlayer].score){
+                                 playersAndScores[everyPlayer].score = player.score;
+                            }
+                            break;
+                        }
+                    }
+                    
+                    //Addig player if it does not exists.
+                    if(!playerExists){
+                        newPlayer = {
+                            "name":player.name,
+                            "score":player.score
+                        };
+                        playersAndScores.push(newPlayer);
+                        localStorage.setItem("scoreboard",JSON.stringify(playersAndScores));
+                    }
+                    
                 }
             },
             addScore: {
                 value: function (playerToAdd) {
-                    // TODO: add playerToAdd in the array
+                    //Adding one score to the current score of the player.
+                    playerToAdd.score += 1;
                 }
             },
             getTopPlayers: {
                 value: function (n) {
-                    // TODO: return top N players as array
+                    // Returning scores of players in ascending order.
+                    playersAndScores = [];
+                    playersAndScores.push(JSON.parse(localStorage.getItem("scoreboard")));
+                    playersAndScores.sort(function(a,b){
+                        return a.score - b.score;
+                    });
+                    return playersAndScores;
                 }
             }
         });
