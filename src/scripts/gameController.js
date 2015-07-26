d06f270a8edaed5142487057c5ce8233a40c9d6d
Atanas Georgiev <atanasgeorgiev83@gmail.com>
2015-07-26 23:36:12 +0300

    Updated game controls.

--- src/scripts/gameController.js
+++ src/scripts/gameController.js
@@ -9,7 +9,9 @@
         gameFieldView = view.fieldView(globals.gameWidth, globals.gameHeight, globals.gameBackground),
         gameControlView = view.controlsView(),
         gameObjects = [],
-        gameRadarRay;
+        gamePaused = false,
+        gameRadarRay,
+        gamePlayer;
 
     function clickEvent() {
         //alert('clicked!');
@@ -18,15 +20,18 @@
     }
 
     function newGameEvent() {
-        alert('New Game');
+        var player = prompt("Please enter your name", "name");
+        gamePlayer = scoreMdl.player(player);
+        initGameObjects();
     }
 
     function pauseResumeEvent() {
-        alert('Pause/Resume Game');
+        gamePaused = !gamePaused;
     }
 
     function exitGameEvent() {
-        alert('Exit Game');
+        initGame();
+        gameRadarRay = undefined;
     }
 
     //implements successful lock
@@ -60,6 +65,62 @@
 
     }
 
+    function initGame() {
+        gameObjects = [];
+        var initText = gameObjectsMdl.landscapeItem(position(globals.gameWidth / 2, globals.gameHeight / 2),
+            size(globals.gameWidth, 100),
+            'images/new_game.png',
+            0, //speed
+            direction.left,
+            0);
+        gameObjects.push(initText);
+    }
+
+    function initGameObjects() {
+        gameObjects = [];
+        // background_tree
+        var testTree = gameObjectsMdl.landscapeItem(position(globals.gameWidth / 2, globals.gameHeight - 120),
+            size(100, 100),
+            'images/tree.png',
+            0, //speed
+            direction.left,
+            0);
+        gameObjects.push(testTree);
+        //Radar (unmovable, simple object)
+        var testRadar = gameObjectsMdl.landscapeItem(position(60, globals.gameHeight - 60),
+            size(100, 100),
+            'images/radar.png',
+            0, //speed
+            direction.left,
+            0);
+        gameObjects.push(testRadar);
+
+        // var testTarget = gameObjectsMdl.enemy(position(globals.gameWidth, globals.gameHeight / 3),
+        //     size(100, 100),
+        //     'images/spaceship.png',
+        //     3.2, //speed
+        //     direction.left,
+        //     0);
+        // gameObjects.push(testTarget);
+
+        var testTarget2 = gameObjectsMdl.enemy(position(globals.gameWidth, globals.gameHeight / 2),
+            size(100, 100),
+            'images/spaceship.png',
+            2, //speed
+            direction.left,
+            0);
+        gameObjects.push(testTarget2);
+
+        //Laser Ray (linked to the radar above)
+        gameRadarRay = gameObjectsMdl.laserRay(45,
+            0.2, //angle speed
+            1, //angle direction
+            88, //max angle
+            2, //min angle
+            3, //range speed
+            testRadar);
+    }
+
     // callbacks
     gameFieldView.registerClickCallback(clickEvent);
     gameControlView.registerNewGameCallback(newGameEvent);
@@ -79,60 +140,28 @@
             direction.left,
             0));
     }
-    
-    // background_tree
-    var testTree = gameObjectsMdl.landscapeItem(position(globals.gameWidth / 2, globals.gameHeight - 120),
-        size(100, 100),
-        'images/tree.png',
-        0, //speed
-        direction.left,
-        0);
-    gameObjects.push(testTree);
-    //Radar (unmovable, simple object)
-    var testRadar = gameObjectsMdl.landscapeItem(position(60, globals.gameHeight - 60),
-        size(100, 100),
-        'images/radar.png',
-        0, //speed
-        direction.left,
-        0);
-    gameObjects.push(testRadar);
-
-    // var testTarget = gameObjectsMdl.enemy(position(globals.gameWidth, globals.gameHeight / 3),
-    //     size(100, 100),
-    //     'images/spaceship.png',
-    //     3.2, //speed
-    //     direction.left,
-    //     0);
-    // gameObjects.push(testTarget);
-
-    var testTarget2 = gameObjectsMdl.enemy(position(globals.gameWidth, globals.gameHeight / 2),
-        size(100, 100),
-        'images/spaceship.png',
-        2, //speed
-        direction.left,
-        0);
-    gameObjects.push(testTarget2);
-
-    //Laser Ray (linked to the radar above)
-    gameRadarRay = gameObjectsMdl.laserRay(45,
-        0.2, //angle speed
-        1, //angle direction
-        88, //max angle
-        2, //min angle
-        3, //range speed
-        testRadar);
 
     // start animation with the objects defined above
     function animate(highResTimestamp) {
         requestAnimationFrame(animate);
-        for (var i = 0; i < gameObjects.length; i++) {
-            gameObjects[i].update();
-            gameRadarRay.update();
+
+        if (!gamePaused) {
+            for (var i = 0; i < gameObjects.length; i++) {
+                gameObjects[i].update();
+                if (gameRadarRay) {
+                    gameRadarRay.update();
+                }
+            }
+            gameFieldView.resetView();
+            if (gameRadarRay) {
+                gameFieldView.draw(gameRadarRay);
+            }
+            gameFieldView.draw(gameObjects);
         }
-        gameFieldView.resetView();
-        gameFieldView.draw(gameRadarRay);
-        gameFieldView.draw(gameObjects);
     }
+
+    // GAME START POINT
+    initGame();
 
     // Start the animation.
     requestAnimationFrame(animate);
