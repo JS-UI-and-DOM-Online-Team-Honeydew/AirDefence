94bc6189e414a0940914061861959f7aa640153b
StanimiraHK <stanimira.kormeva.4@gmail.com>
2015-07-30 14:38:06 +0300

    Objects tests finished

--- tests/gameObjectModel.Tests.js
+++ tests/gameObjectModel.Tests.js
@@ -1,4 +1,4 @@
-var ObjectsModel = require('../src/scripts/gameObjectsModel.js')();
+var ObjectsModel = require('../src/scripts/gameObjectsModel.js');
 var expect = require('chai').expect;
 
 
@@ -14,7 +14,6 @@ describe('Test for objects in the game.', function () {
                     },
                     speed: 4,
                     direction: {x:0, y:-1},
-                    isTarget: true,
                     zIndex: 1
                 },
                 INVALID: {
@@ -27,104 +26,350 @@ describe('Test for objects in the game.', function () {
                     speed: true,
                     size: {width:-20, height:700},
                     direction: {x:5, y:-1},
-                    isTarget: 'true',
                     zIndex: 'hit you'
                 }
             }
 
-    describe('Enemy tests.', function () {
+    describe('Enemy tests:', function () {
+        var Enemy = function() {
+            var enemy = Object.create(ObjectsModel.enemy).init(CONSTS.VALID.position, 
+                    CONSTS.VALID.size, 
+                    CONSTS.VALID.image,
+                    CONSTS.VALID.speed,
+                    CONSTS.VALID.direction,
+                    CONSTS.VALID.zIndex);
+            return enemy;
+        }
 
-        it('expect enemy to be an object', function () {
-            expect(ObjectsModel.enemy).to.be.a('function');
+        it('1.expect enemy to be an function', function () {
+            expect(Enemy).to.be.a('function');
         }),
 
-        it('expect enemy to throw position error', function () {
+        it('2.expect enemy to throw position error', function () {
             function positionError () {
                 Object.create(ObjectsModel.enemy).init(CONSTS.INVALID.position, 
                     CONSTS.VALID.size, 
                     CONSTS.VALID.image,
                     CONSTS.VALID.speed,
                     CONSTS.VALID.direction,
-                    CONSTS.VALID.zIndex,
-                    CONSTS.VALID.isTarget);
+                    CONSTS.VALID.zIndex);
             }
             expect(positionError).to.throw();
         }),
         
-        it('expect enemy to throw size error', function () {
+        it('3.expect enemy to throw size error', function () {
             function sizeError () {
                 Object.create(ObjectsModel.enemy).init(CONSTS.VALID.position, 
                     CONSTS.INVALID.size, 
                     CONSTS.VALID.image,
                     CONSTS.VALID.speed,
                     CONSTS.VALID.direction,
-                    CONSTS.VALID.zIndex,
-                    CONSTS.VALID.isTarget);
+                    CONSTS.VALID.zIndex);
             }
             expect(sizeError).to.throw();
         }),
 
-        it('expect enemy to throw speed error', function() {
+        it('4.expect enemy to throw speed error', function() {
             function speedError () {
                 Object.create(ObjectsModel.enemy).init(CONSTS.VALID.position, 
                     CONSTS.VALID.size, 
                     CONSTS.VALID.image,
                     CONSTS.INVALID.speed,
                     CONSTS.VALID.direction,
-                    CONSTS.VALID.zIndex,
-                    CONSTS.VALID.isTarget);
+                    CONSTS.VALID.zIndex);
             }
             expect(speedError).to.throw();
         }),
 
-        it('expect enemy to throw speed error', function() {
+        it('5.expect enemy to throw speed error', function() {
             function directionError () {
                 Object.create(ObjectsModel.enemy).init(CONSTS.VALID.position, 
                     CONSTS.VALID.size, 
                     CONSTS.VALID.image,
                     CONSTS.VALID.speed,
                     CONSTS.INVALID.direction,
-                    CONSTS.VALID.zIndex,
-                    CONSTS.VALID.isTarget);
+                    CONSTS.VALID.zIndex);
             }
             expect(directionError).to.throw();
         }),
 
-        it('expect enemy to throw speed error', function() {
+        it('6.expect enemy to throw speed error', function() {
             function zIndexError () {
                 Object.create(ObjectsModel.enemy).init(CONSTS.VALID.position, 
                     CONSTS.VALID.size, 
                     CONSTS.VALID.image,
                     CONSTS.VALID.speed,
                     CONSTS.VALID.direction,
-                    CONSTS.INVALID.zIndex,
-                    CONSTS.VALID.isTarget);
+                    CONSTS.INVALID.zIndex);
             }
             expect(zIndexError).to.throw();
-        }),
+        })
+    }),
 
-        it('expect enemy to throw speed error', function() {
-            function isTargetError () {
-                Object.create(ObjectsModel.enemy).init(CONSTS.VALID.position, 
+    describe('Bomb tests:', function(){
+        var Bomb = function() {
+            var bomb = Object.create(ObjectsModel.bomb).init(CONSTS.VALID.position, 
                     CONSTS.VALID.size, 
                     CONSTS.VALID.image,
                     CONSTS.VALID.speed,
                     CONSTS.VALID.direction,
                     CONSTS.VALID.zIndex,
-                    CONSTS.INVALID.isTarget);
-            }
-            expect(isTargetError).to.throw();
+                    6);
+            return bomb;
+        }
+
+
+              it('1.expect bomb to be an function', function () {
+            expect(Bomb).to.be.a('function');
         }),
 
-        it('expect enemy.update to be a function', function (){
-            expect(ObjectsModel.enemy.update).to.be.a(function)
+        it('2.expect bomb to throw position error', function () {
+            function positionError () {
+                Object.create(ObjectsModel.bomb).init(CONSTS.INVALID.position, 
+                    CONSTS.VALID.size, 
+                    CONSTS.VALID.image,
+                    CONSTS.VALID.speed,
+                    CONSTS.VALID.direction,
+                    CONSTS.VALID.zIndex,
+                    6);
+            }
+            expect(positionError).to.throw();
+        }),
+        
+        it('3.expect bomb to throw size error', function () {
+            function sizeError () {
+                Object.create(ObjectsModel.bomb).init(CONSTS.VALID.position, 
+                    CONSTS.INVALID.size, 
+                    CONSTS.VALID.image,
+                    CONSTS.VALID.speed,
+                    CONSTS.VALID.direction,
+                    CONSTS.VALID.zIndex,
+                    6);
+            }
+            expect(sizeError).to.throw();
+        }),
+
+        it('4.expect bomb to throw speed error', function() {
+            function speedError () {
+                Object.create(ObjectsModel.bomb).init(CONSTS.VALID.position, 
+                    CONSTS.VALID.size, 
+                    CONSTS.VALID.image,
+                    CONSTS.INVALID.speed,
+                    CONSTS.VALID.direction,
+                    CONSTS.VALID.zIndex,
+                    6);
+            }
+            expect(speedError).to.throw();
+        }),
+
+        it('5.expect bomb to throw speed error', function() {
+            function directionError () {
+                Object.create(ObjectsModel.bomb).init(CONSTS.VALID.position, 
+                    CONSTS.VALID.size, 
+                    CONSTS.VALID.image,
+                    CONSTS.VALID.speed,
+                    CONSTS.INVALID.direction,
+                    CONSTS.VALID.zIndex,
+                    6);
+            }
+            expect(directionError).to.throw();
+        }),
+
+        it('6.expect bomb to throw speed error', function() {
+            function zIndexError () {
+                Object.create(ObjectsModel.bomb).init(CONSTS.VALID.position, 
+                    CONSTS.VALID.size, 
+                    CONSTS.VALID.image,
+                    CONSTS.VALID.speed,
+                    CONSTS.VALID.direction,
+                    CONSTS.INVALID.zIndex,
+                    6);
+            }
+            expect(zIndexError).to.throw();
+        })
+    }),
+    
+    describe('AeroBomb tests:', function(){
+        var testRadar = function() {
+            var radar = Object.create(ObjectsModel.landscapeItem).init(CONSTS.VALID.position,
+                CONSTS.VALID.size,
+                CONSTS.VALID.image,
+                0, 
+                CONSTS.VALID.direction,
+                0);
+
+            return radar;
+        }
+        var gameRadarRay = function() {
+            var radarRey = Object.create(ObjectsModel.laserRay).init(4,
+                0.1, 
+                1, 
+                88, 
+                2, 
+                1, 
+                testRadar);
+
+            return radarRey;
+        }
+
+        var AeroBomb = function() {
+            var aeroBomb = Object.create(ObjectsModel.enemy).init(CONSTS.VALID.position, 
+                    CONSTS.VALID.size, 
+                    CONSTS.VALID.image,
+                    CONSTS.VALID.speed,
+                    CONSTS.VALID.direction,
+                    CONSTS.VALID.zIndex,
+                    gameRadarRay);
+            return aeroBomb;
+        }
+
+
+      it('1.expect aeroBomb to be an function', function () {
+            expect(AeroBomb).to.be.a('function');
+        }),
+
+        it('2.expect aeroBomb to throw position error', function () {
+            function positionError () {
+                Object.create(ObjectsModel.aeroBomb).init(CONSTS.INVALID.position, 
+                    CONSTS.VALID.size, 
+                    CONSTS.VALID.image,
+                    CONSTS.VALID.speed,
+                    CONSTS.VALID.direction,
+                    CONSTS.VALID.zIndex,
+                    CgameRadarRay);
+            }
+            expect(positionError).to.throw();
+        }),
+        
+        it('3.expect aeroBomb to throw size error', function () {
+            function sizeError () {
+                Object.create(ObjectsModel.aeroBomb).init(CONSTS.VALID.position, 
+                    CONSTS.INVALID.size, 
+                    CONSTS.VALID.image,
+                    CONSTS.VALID.speed,
+                    CONSTS.VALID.direction,
+                    CONSTS.VALID.zIndex,
+                    gameRadarRay);
+            }
+            expect(sizeError).to.throw();
+        }),
+
+        it('4.expect aeroBomb to throw speed error', function() {
+            function speedError () {
+                Object.create(ObjectsModel.aeroBomb).init(CONSTS.VALID.position, 
+                    CONSTS.VALID.size, 
+                    CONSTS.VALID.image,
+                    CONSTS.INVALID.speed,
+                    CONSTS.VALID.direction,
+                    CONSTS.VALID.zIndex,
+                    gameRadarRay);
+            }
+            expect(speedError).to.throw();
+        }),
+
+        it('5.expect aeroBomb to throw speed error', function() {
+            function directionError () {
+                Object.create(ObjectsModel.aeroBomb).init(CONSTS.VALID.position, 
+                    CONSTS.VALID.size, 
+                    CONSTS.VALID.image,
+                    CONSTS.VALID.speed,
+                    CONSTS.INVALID.direction,
+                    CONSTS.VALID.zIndex,
+                    gameRadarRay);
+            }
+            expect(directionError).to.throw();
+        }),
+
+        it('6.expect aeroBomb to throw speed error', function() {
+            function zIndexError () {
+                Object.create(ObjectsModel.aeroBomb).init(CONSTS.VALID.position, 
+                    CONSTS.VALID.size, 
+                    CONSTS.VALID.image,
+                    CONSTS.VALID.speed,
+                    CONSTS.VALID.direction,
+                    CONSTS.INVALID.zIndex,
+                    gameRadarRay);
+            }
+            expect(zIndexError).to.throw();
         })
     }),
 
-    describe('Test', function(){
-      
-    });
-    
+    describe('LandscapeItem tests:', function(){
+        var LandscapeItem = function() {
+            var landscapeItem = Object.create(ObjectsModel.bomb).init(CONSTS.VALID.position, 
+                    CONSTS.VALID.size, 
+                    CONSTS.VALID.image,
+                    CONSTS.VALID.speed,
+                    CONSTS.VALID.direction,
+                    CONSTS.VALID.zIndex);
+            return landscapeItem;
+        }
+
+
+              it('1.expect landscapeItem to be an function', function () {
+            expect(LandscapeItem).to.be.a('function');
+        }),
+
+        it('2.expect landscapeItem to throw position error', function () {
+            function positionError () {
+                Object.create(ObjectsModel.bomb).init(CONSTS.INVALID.position, 
+                    CONSTS.VALID.size, 
+                    CONSTS.VALID.image,
+                    CONSTS.VALID.speed,
+                    CONSTS.VALID.direction,
+                    CONSTS.VALID.zIndex);
+            }
+            expect(positionError).to.throw();
+        }),
+        
+        it('3.expect landscapeItem to throw size error', function () {
+            function sizeError () {
+                Object.create(ObjectsModel.bomb).init(CONSTS.VALID.position, 
+                    CONSTS.INVALID.size, 
+                    CONSTS.VALID.image,
+                    CONSTS.VALID.speed,
+                    CONSTS.VALID.direction,
+                    CONSTS.VALID.zIndex);
+            }
+            expect(sizeError).to.throw();
+        }),
+
+        it('4.expect landscapeItem to throw speed error', function() {
+            function speedError () {
+                Object.create(ObjectsModel.bomb).init(CONSTS.VALID.position, 
+                    CONSTS.VALID.size, 
+                    CONSTS.VALID.image,
+                    CONSTS.INVALID.speed,
+                    CONSTS.VALID.direction,
+                    CONSTS.VALID.zIndex);
+            }
+            expect(speedError).to.throw();
+        }),
+
+        it('5.expect landscapeItem to throw speed error', function() {
+            function directionError () {
+                Object.create(ObjectsModel.bomb).init(CONSTS.VALID.position, 
+                    CONSTS.VALID.size, 
+                    CONSTS.VALID.image,
+                    CONSTS.VALID.speed,
+                    CONSTS.INVALID.direction,
+                    CONSTS.VALID.zIndex);
+            }
+            expect(directionError).to.throw();
+        }),
+
+        it('6.expect bomb to throw speed error', function() {
+            function zIndexError () {
+                Object.create(ObjectsModel.bomb).init(CONSTS.VALID.position, 
+                    CONSTS.VALID.size, 
+                    CONSTS.VALID.image,
+                    CONSTS.VALID.speed,
+                    CONSTS.VALID.direction,
+                    CONSTS.INVALID.zIndex);
+            }
+            expect(zIndexError).to.throw();
+        });
+    });   
 });
         
 
